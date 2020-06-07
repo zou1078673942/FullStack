c54c758ebf410e719fe876686fbdc09e0dda2853
zou1078673942 <1078673942@qq.com>
2020-06-07 14:24:29 +0800

    React

--- react/comment-book/src/Comment.js
+++ react/comment-book/src/Comment.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import CommentInput from './CommentInput'
 
 class Comment extends Component {
   constructor () {
@@ -10,9 +10,10 @@ class Comment extends Component {
 
   render () {
     return (
-      <div className='comment'>
-        111
-      </div>
+        <div>
+            {this.props.comment.content}
+        </div>
+        
     )
   }
 }
--- react/comment-book/src/CommentList.js
+++ react/comment-book/src/CommentList.js
@@ -29,5 +29,4 @@ class CommentList extends Component {
   }
 }
 
-
 export default CommentList
