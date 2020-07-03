acc589ee8e9c905be71cbc7244307e1fe74b1a5a
zou1078673942 <1078673942@qq.com>
2020-07-03 09:37:06 +0800

    distribute-array

--- codewars/distribute-array/1.js
+++ codewars/distribute-array/1.js
@@ -1,8 +1,29 @@
 const distributeEvenly = (array) => {
-    let rtn = [];
-    array.forEach(element => {
-        
-    });
-    return rtn;
+    let ans = [];
+    let s = [];
+    for (let i = 0; i < array.length; i++){
+        if(s.indexOf(array[i]) == -1){
+            s.push(array[i]);
+        }
+    }
+    do{
+        let arr = [];
+        for (let i = 0; i < s.length;i++){
+            if(array.length !=0 && array.indexOf(s[i]) != -1){
+                arr.push(s[i]);
+                let ar = [];
+                for(let j=0; j<array.length; j++){
+                    if(j != array.indexOf(s[i])){
+                        ar.push(array[j])
+                    }
+                }
+                array = ar;
+            }
+        }
+        for (let i=0;i < arr.length;i++){
+            ans.push(arr[i])
+        }
+    }while(array.length > 0)
+    return ans;
 };
 console.log(distributeEvenly(['one', 'one', 'two', 'two', 'three', 'three', 'four', 'four']))
--- codewars/distribute-array/readme.md
+++ codewars/distribute-array/readme.md
@@ -1,5 +1,5 @@
 ## 
-- https://www.codewars.com/kata/5bb50eb68f8bbdb4b300021d/train/javascript
+https://www.codewars.com/kata/5bb50eb68f8bbdb4b300021d/train/javascript
 - Your distributeEvenly function will take an array as an argument and needs to return a new array with the values distributed evenly.
 - 接收一个数组作为参数，并返回一个数组，将值平均分布，即 1 2 3 3 4 2 1 返回 1 2 3 4 1 2 3
 
