2ac63bc47c72720dd5fb3fce822549b001a23bd3
zou1078673942 <1078673942@qq.com>
2020-07-06 20:57:43 +0800

    codewars

---
+++ codewars/Braking well/readme.md
@@ -0,0 +1,2 @@
+## Braking well
+https://www.codewars.com/kata/565c0fa6e3a7d39dee000125
--- codewars/Equal Sides Of An Array/1.js
+++ codewars/Equal Sides Of An Array/1.js
@@ -1,5 +1,10 @@
 function findEvenIndex(arr)
 {
-  return arr.findIndex((i,a)=> a.slice(0,i).reduce((p,c)=>p+c,0)==a.slice(i+1).reduce((p,c)=>p+c,0));
+  for(let i=1;i<=arr.length;i++){
+    console.log(arr.slice(0,i),arr.slice(i-1))
+    if (arr.slice(0,i).reduce((a,b=0) => a+b) === arr.slice(i-1).reduce((a,b=0) => a+b))
+      return i-1
+  }
+  return -1
 }
-console.log(findEvenIndex[1,2])
+console.log(findEvenIndex([10,-80,10,10,15,35,20]))
