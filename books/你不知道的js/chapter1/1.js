0f22a637914e2fc5be575d22951000999181cb02
zou1078673942 <1078673942@qq.com>
2020-07-13 16:25:58 +0800

    js

---
+++ books/你不知道的js/chapter1/1.js
@@ -0,0 +1,6 @@
+function func(){
+    var a = 1
+    var a = 2
+    console.log(a)
+}
+func()
---
+++ books/你不知道的js/chapter1/2.js
@@ -0,0 +1,10 @@
+function a(age){
+    console.log(age)
+    var age = 20
+    console.log(age)
+    age = function (){
+        
+    }
+    console.log(age)
+}
+a(18)
