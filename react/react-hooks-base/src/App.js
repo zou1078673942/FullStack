f22e6bd444b2cf265c1c0a3c6ec55c5edec10c4b
zou1078673942 <1078673942@qq.com>
2020-07-13 14:41:37 +0800

    react

---
+++ react/react-hooks-base/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DemoUseEffect from './UseEffect'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <DemoUseEffect/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/react-hooks-base/src/UseEffect.jsx
@@ -0,0 +1,37 @@
+import React,{ useEffect, useState, useMemo} from 'react';
+import { useCallback } from 'react';
+
+function DemoUseEffect(){
+    const [inputValue, setVal] = useState('11')
+    const [list, setList] = useState('')
+    // const [val, setVal] = ['', () => {}]
+    const arr = [11,22]
+    const val = useMemo(() => arr)
+    console.log(val === arr)
+    const handleChange = useCallback((event) => {
+        setVal(event.target.value);
+    })
+    useEffect(() => {
+        fetch('http://neteasecloudmusicapi.zhaoboy.com/search?keywords='+inputValue)
+        .then(res => res.json())
+        .then((res) => {
+            setList(res.result.songs)
+        })
+        return () => {
+            // 卸载
+            // clearInterval()
+            // document.removeEventListener()
+        }
+    },[inputValue])
+    // 在 componentDidMount componentDidUpdate componentWillUnmount 三个时机运行
+    // 传递一个 依赖   
+    // react 会检查依赖中数据是否发生变化  如果没有变化就不会重新调用
+    // 只执行一次类似 componentDidMount
+    // 执行多次的话 componentDidUpdate
+    return (
+        <div>
+            <input type="text" value={inputValue} onChange={handleChange}/>
+        </div>
+    )
+}
+export default DemoUseEffect
