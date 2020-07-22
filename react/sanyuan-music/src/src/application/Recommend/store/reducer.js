f2d168ba6083da3d410b5b1fe41d86ed0577f20e
zou1078673942 <1078673942@qq.com>
2020-07-22 11:39:21 +0800

    styled

---
+++ react/sanyuan-music/src/src/application/Recommend/store/reducer.js
@@ -0,0 +1,11 @@
+// import * as actionTypes from './constant';
+
+const defaultState = {
+  recommendList: []
+}
+export default (state = defaultState, action) => {
+  switch(action.type) {
+    default:
+        return state
+  }
+}
