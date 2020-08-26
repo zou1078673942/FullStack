33dfe23b598b3d0308d8860acc845d8370ef2cbf
zou1078673942 <1078673942@qq.com>
2020-08-26 11:46:18 +0800

    vue

--- vue/element-code/src/components/ElButton.vue
+++ vue/element-code/src/components/ElButton.vue
@@ -1,43 +1,76 @@
 <template>
-<button
-    @click="handleClick"
-    class="el-button"
-    :class="[
-        type?'el-button--'+type:'',
-        {
-            'is-round':round,
-            'is-circle':circle
-        }
-    ]"
-    >
-    <!-- {{type}} - {{tit}} -->
-    <slot></slot>
-</button>
+    <button
+        @click="handleClick"
+        class="el-button"
+        :disabled="loading"
+        :type="nativeType"
+        :autofocus="autofocus"
+        :class="[
+            type?'el-button--'+type:'',
+            buttonSize?'el-button--'+buttonSize:'',
+            {
+                'is-loading':loading,
+                'is-round':round,
+                'is-circle':circle,
+                'is-disabled':buttonDisabled,
+                'is-plain':plain
+            }
+        ]"
+        >
+        <i class="el-icon-loading" v-if="loading"></i>
+        <i :class="icon" v-if="icon"></i>
+        <!-- {{type}} - {{tit}} -->
+        <slot v-if="!loading && !icon"></slot>
+    </button>
 </template>
 <script>
-export default {
-    data(){
-        return {
-            tit:'hello'
-        }
-    },
-    props:{
-        type:{
-            type:String,
-            default:'default'
+    export default {
+        name:'ElButton',
+        data(){
+            return {
+                tit:'hello',
+            }
         },
-        round: Boolean,
-        circle: Boolean
-    },
-    created() {
-        console.log(this.type,this.data)
-    },
-    methods:{
-        handleClick(evt){
-            this.$emit('click',evt)
+        props:{
+            nativeType:{
+                type:String,
+                default:'button'
+            },
+            type:{
+                type:String,
+                default:'default'
+            },
+            round: Boolean,
+            circle: Boolean,
+            icon: {
+                icon: String,
+                default:''
+            },
+            loading: Boolean,
+            autofocus: Boolean,
+            plain: Boolean,
+            size:{
+                type:String,
+                default:''
+            }
+        },
+        computed:{
+            buttonDisabled() {
+                return this.disabled
+            },
+            buttonSize() {
+                return this.size
+            }
+        },
+        created() {
+            console.log(this.type,this.data)
+        },
+        methods:{
+            handleClick(evt){
+                console.log(evt)
+            }
         }
     }
-}
 </script>
 <style>
 </style>
